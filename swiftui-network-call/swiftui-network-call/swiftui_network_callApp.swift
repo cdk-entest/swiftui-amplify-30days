03427ed2fcad9b38aeceaf2b313ceadf432092bb
hai tran <hai@entest.io>
2022-10-14 15:10:34 +0700

    network call

---
+++ swiftui-network-call/swiftui-network-call/swiftui_network_callApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_network_callApp.swift
+//  swiftui-network-call
+//
+//  Created by MINH HAI, TRAN on 14/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_network_callApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
